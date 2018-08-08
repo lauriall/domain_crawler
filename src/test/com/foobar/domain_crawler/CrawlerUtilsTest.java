4fa3d9d152866967eff56fb042f6b3bb4d3dc828
lauria <lauri.all@reach-u.com>
2018-08-08 17:03:44 +0300

    test

---
+++ src/test/com/foobar/domain_crawler/CrawlerUtilsTest.java
@@ -0,0 +1,14 @@
+package java.com.foobar.domain_crawler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CrawlerUtilsTest {
+
+    @Test
+    public void testQuery()  {
+        assertTrue(true);
+    }
+
+}
